5163d20c6071e009af78b6abd1ee8f38e6a50f4b
zFaka <fakakill2000@gmail.com>
2021-01-27 16:33:49 -0300

    Updates

---
+++ precache-manifest.923784bd3fb9f0a6290104d599f4e08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb2167e05955543caf53a5a89e578c5",
+    "url": "/journld/index.html"
+  },
+  {
+    "revision": "947afb183c4dba6f3670",
+    "url": "/journld/static/css/main.7b28ae07.chunk.css"
+  },
+  {
+    "revision": "9271fac4bc6566c264b3",
+    "url": "/journld/static/js/2.3403de19.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/journld/static/js/2.3403de19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947afb183c4dba6f3670",
+    "url": "/journld/static/js/main.c0c99134.chunk.js"
+  },
+  {
+    "revision": "f2c7f22f422ee69b21e7",
+    "url": "/journld/static/js/runtime-main.38ab8744.js"
+  }
+]);
